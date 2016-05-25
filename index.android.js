669f54957150121363cc302233c300ef49ca82ec
ted <linyc1983@gmail>
2016-05-25 19:22:19 +0800

    add movie list part1

--- index.android.js
+++ index.android.js
@@ -6,25 +6,107 @@
 import React, { Component } from 'react';
 import {
   AppRegistry,
+  Image,
   StyleSheet,
   Text,
   View
 } from 'react-native';
 
+var MOCKED_MOVIES_DATA = [
+  {title: 'Title', year: '2015', posters: {thumbnail: 'http://i.imgur.com/UePbdph.jpg'}},
+];
+
+var API_KEY = '7waqfqbprs7pajbz28mqf6vz';
+var API_URL = 'http://api.rottentomatoes.com/api/public/v1.0/lists/movies/in_theaters.json';
+var PAGE_SIZE = 25;
+var PARAMS = '?apikey=' + API_KEY + '&page_limit=' + PAGE_SIZE;
+var REQUEST_URL = API_URL + PARAMS;
+
 class myApp extends Component {
-  render() {
+
+ // getInitialState: function() {
+ //    return {
+ //      movies: null,
+ //    };
+ //  },
+
+/*  getInitialState() {
+    return {
+      movies: null,
+    };
+  }*/
+
+  constructor(props) {
+    super(props);
+    this.state = {movies: null};
+  }
+
+  componentDidMount() {
+    // console.log('yeah, seriously.');
+    this.fetchData();
+  }
+
+  fetchData() {
+    fetch(REQUEST_URL)
+      .then((response) => response.json())
+      .then((responseData) => {
+        // console.log('yeah, seriously.');
+        this.setState({
+          movies: responseData.movies,
+        });
+      })
+      .done();
+  }
+
+
+  render() {    
+    if (!this.state.movies) {
+      return this.renderLoadingView();
+    }
+
+    // var movie = MOCKED_MOVIES_DATA[0];
+    var movie = this.state.movies[0];
+    
+    console.log('yeah, seriously:' + this.state.movies.length);
+    
+    return this.renderMovie(movie);
+    /*return (
+      <View style={styles.container}>
+        <Image
+          source={{uri: movie.posters.thumbnail}}
+          style={styles.thumbnail}
+        />
+        <View style={styles.rightContainer}>
+          <Text style={styles.title}>{movie.title}</Text>
+          <Text style={styles.year}>{movie.year}</Text>
+        </View>
+      </View>
+    );*/
+
+  }
+
+
+  renderLoadingView() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native! testtest
-	  {console.log('yeah, seriously.')}
+        <Text>
+          Loading movies...
         </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.android.js
-        </Text>
-        <Text style={styles.instructions}>
-          Shake or press menu button for dev menu
-        </Text>
+      </View>
+    );
+  }
+
+  renderMovie(movie) {
+    return (
+      <View style={styles.container}>
+        <Image
+          source={{uri: movie.posters.thumbnail}}
+          style={styles.thumbnail}
+        />
+        <View style={styles.rightContainer}>
+          <Text style={styles.title}>{movie.title}</Text>
+          <Text style={styles.year}>{movie.year}</Text>
+        </View>
       </View>
     );
   }
@@ -33,6 +115,7 @@ class myApp extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    flexDirection: 'row',
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: '#F5FCFF',
@@ -46,6 +129,21 @@ const styles = StyleSheet.create({
     textAlign: 'center',
     color: '#333333',
     marginBottom: 5,
+  },
+  thumbnail: {
+    width: 53,
+    height: 81,
+  },
+  rightContainer: {
+    flex: 1,
+  },
+  title: {
+    fontSize: 20,
+    marginBottom: 8,
+    textAlign: 'center',
+  },
+  year: {
+    textAlign: 'center',
   },
 });
 
